3788c81dbcd55f9aff3fbdce2024f8da999eb2fc
Iakovos Mastrogiannopoulos <iakmastro@outlook.com.gr>
2022-06-27 22:39:18 +0300

    temp

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import {NgModule}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
-import {AppRoutingModule}           from './app-routing.module';
-import {AppComponent}               from './app.component';
-import {NgbModule}                  from '@ng-bootstrap/ng-bootstrap';
-import {FormsModule}                from "@angular/forms";
-import {UsersModule}                from "./users/users.module";
-import {AngularFireModule}          from "@angular/fire/compat";
-import {environment}                from "../environments/environment";
+import {AppRoutingModule}                       from './app-routing.module';
+import {AppComponent}                           from './app.component';
+import {NgbModule}                              from '@ng-bootstrap/ng-bootstrap';
+import {FormsModule}                            from "@angular/forms";
+import {UsersModule}                            from "./users/users.module";
+import {AngularFireModule}                      from "@angular/fire/compat";
+import {environment}                            from "../environments/environment";
 import {AngularFireAuthModule}                  from "@angular/fire/compat/auth";
 import {firebase, firebaseui, FirebaseUIModule} from "firebaseui-angular";
 
